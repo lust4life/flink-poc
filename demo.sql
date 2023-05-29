d4823c911c440c87d83de3a75a7a796596aca1ff
Jun <lust4life.jun@gmail.com>
2023-05-29 11:57:27 +0800

    paimon

--- demo.sql
+++ demo.sql
@@ -1,37 +1,13 @@
--- register a PostgreSQL table 'orders' in Flink SQL
-CREATE TABLE orders (
-  order_id STRING,
-  product_id STRING,
-  customer_id STRING,
-  purchase_timestamp TIMESTAMP
-) WITH (
-  'connector' = 'postgres-cdc',
-  'hostname' = 'postgres',
-  'port' = '5432',
-  'username' = 'postgres',
-  'password' = 'postgres',
-  'database-name' = 'postgres',
-  'schema-name' = 'public',
-  'table-name' = 'orders',
-  'decoding.plugin.name' = 'pgoutput'
-);
+-- CREATE CATALOG pg WITH(
+--     'type' = 'jdbc',
+--     'default-database' = 'postgres',
+--     'username' = 'postgres',
+--     'password' = 'postgres',
+--     'base-url' = 'jdbc:postgresql://postgres:5432'
+-- );
 
-CREATE TABLE products (
-  product_id STRING,
-  product_name STRING,
-  sale_price INT,
-  product_rating DOUBLE
-) WITH (
-  'connector' = 'postgres-cdc',
-  'hostname' = 'postgres',
-  'port' = '5432',
-  'username' = 'postgres',
-  'password' = 'postgres',
-  'database-name' = 'postgres',
-  'schema-name' = 'public',
-  'table-name' = 'products',
-  'decoding.plugin.name' = 'pgoutput'
-);
+-- USE CATALOG pg;
+
 
 CREATE CATALOG ods WITH (
     'type' = 'paimon',
@@ -44,7 +20,45 @@ CREATE TABLE orders (
   order_id STRING,
   product_id STRING,
   customer_id STRING,
-  purchase_timestamp TIMESTAMP
+  purchase_timestamp TIMESTAMP,
+  PRIMARY KEY (order_id) NOT ENFORCED
+);
+
+-- register a PostgreSQL table 'orders' in Flink SQL
+CREATE TABLE default_catalog.default_database.orders (
+  order_id STRING,
+  product_id STRING,
+  customer_id STRING,
+  purchase_timestamp TIMESTAMP,
+  PRIMARY KEY (order_id) NOT ENFORCED
+) WITH (
+  'connector' = 'postgres-cdc',
+  'hostname' = 'postgres',
+  'port' = '5432',
+  'username' = 'postgres',
+  'password' = 'postgres',
+  'database-name' = 'postgres',
+  'schema-name' = 'public',
+  'table-name' = 'orders',
+  'decoding.plugin.name' = 'pgoutput'
+);
+
+CREATE TABLE default_catalog.default_database.products (
+  product_id STRING,
+  product_name STRING,
+  sale_price INT,
+  product_rating DOUBLE,
+  PRIMARY KEY (product_id) NOT ENFORCED
+) WITH (
+  'connector' = 'postgres-cdc',
+  'hostname' = 'postgres',
+  'port' = '5432',
+  'username' = 'postgres',
+  'password' = 'postgres',
+  'database-name' = 'postgres',
+  'schema-name' = 'public',
+  'table-name' = 'products',
+  'decoding.plugin.name' = 'pgoutput'
 );
 
 insert into orders select * from default_catalog.default_database.orders;
